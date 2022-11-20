3958976881556009fe34c1203edf0e3f5f7f5cf2
Chiến Nguyễn <nmchien99@gmail.com>
2022-11-20 14:26:01 +0700

    upload screenshot

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a658882ef4c85465696ef17e14a4db0d-1764417393.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a87e07cfb096c464fa6b3ec8ca1ded13-81648873.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
