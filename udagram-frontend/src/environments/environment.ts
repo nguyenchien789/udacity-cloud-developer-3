0c168594727786bbc3151bd86242fccc3887d9f3
Chiến Nguyễn <nmchien99@gmail.com>
2022-11-26 14:57:23 +0700

    new version

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a87e07cfb096c464fa6b3ec8ca1ded13-81648873.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
